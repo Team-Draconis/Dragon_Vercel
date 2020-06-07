7c3b92e2bc901ffaaa187158f7b9516face11777
Mako Kusuda <mako.kusuda@gmail.com>
2020-06-07 14:59:44 +0900

    Fix

--- pages/api/testRunner.js
+++ pages/api/testRunner.js
@@ -1,24 +1,48 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+// const util = require("util");
+// const exec = util.promisify(require("child_process").exec);
+const { exec } = require("child_process");
 
 export default async (req, res) => {
   const { method } = req;
   console.log(method);
   switch (method) {
+    // case "POST":
+    //   try {
+    //     // async function testExec() {
+    //     console.log("trying to post");
+
+    //     const { stdout, stderr } = await exec("yarn test");
+    //     console.log(stderr);
+    //     // res.status(200).json({ data: stderr });
+    //     res.status(200).json({ message: "success" });
+    //     // }
+    //     // testExec();
+    //   } catch (error) {
+    //     console.log("error caught");
+    //     res.status(400).json({ message: "Failure" });
+    //   }
+    //   break;
+
     case "POST":
       try {
-        // async function testExec() {
-        console.log("trying to post");
+        console.log(req.body, "Code in API Folder");
+        // let result;
+        exec("yarn test", (error, stdout, stderr) => {
+          if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`);
+          }
+          console.log(`stdout: ${stdout}`);
+          res.status(200).json({ data: stderr });
+          // result = stdout;
+        });
 
-        const { stdout, stderr } = await exec("yarn test");
-        console.log(stderr);
-        // res.status(200).json({ data: stderr });
-        res.status(200).json({ message: "success" });
-        // }
-        // testExec();
+        // res.status(200).json({ data: result });
       } catch (error) {
-        console.log("error caught");
-        res.status(400).json({ message: "Failure" });
+        res.status(400).json({ success: false });
       }
       break;
 
