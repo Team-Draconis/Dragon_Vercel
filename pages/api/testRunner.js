1a15b4a38a310bdeb6a0f1e421e559064bcd4d57
Mako Kusuda <mako.kusuda@gmail.com>
2020-06-07 14:42:06 +0900

    Fix

--- pages/api/testRunner.js
+++ pages/api/testRunner.js
@@ -1,6 +1,12 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
+async function takeOutStderr() {
+  const { stdout, stderr } = await exec("yarn test");
+  const info = stderr;
+  return info;
+}
+
 export default async (req, res) => {
   const { method } = req;
   console.log(method);
@@ -18,7 +24,7 @@ export default async (req, res) => {
         // testExec();
       } catch (error) {
         console.log("error caught");
-        res.status(400).json({ success: false });
+        res.status(400).json({ message: "Failure" });
       }
       break;
 
@@ -26,6 +32,14 @@ export default async (req, res) => {
       try {
         console.log(req.body, "Code in API Folder");
         res.status(200).json({ success: true });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "VIEW":
+      try {
+        let result = await takeOutStderr();
+        res.status(200).json({ data: result });
       } catch (error) {
         res.status(400).json({ success: false });
       }
