9bb0c8a0b9f386a5858b62d57491302b11d941b0
cwcraw <cwcraw@gmail.com>
2020-06-09 13:00:34 +0900

    premerge commit

--- src/ToggleMessage.test.js
+++ src/ToggleMessage.test.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-<<<<<<< HEAD
 // import ToggleMessage from "./ToggleMessage";
-=======
-import ToggleMessage from "./ToggleMessage";
->>>>>>> 909bbd403faa2ee68f1934e759c9f157d5959411
+
 import "@testing-library/jest-dom/extend-expect";
 
 test("Render ToggleMessage component and check if it has text:'Want to buy a new car?'", () => {
