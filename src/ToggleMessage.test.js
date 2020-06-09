88410552891c2448249ab9a45a41dcb56bc3ddec
Mako Kusuda <mako.kusuda@gmail.com>
2020-06-09 15:44:37 +0900

    Add test - ToggleMessage and AddingCalculator

--- src/ToggleMessage.test.js
+++ src/ToggleMessage.test.js
@@ -1,10 +1,55 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import ToggleMessage from "./ToggleMessage";
 import "@testing-library/jest-dom/extend-expect";
-import Codes from "../tester";
 
-test("Render Codes component and check if it has text:'Hello World!'", () => {
-  const { getByText } = render(<Codes />);
-  const linkElement = getByText(/Hello World!/i);
-  expect(linkElement).toBeInTheDocument();
+test("Toggle message should be set data-testid='toggle'", () => {
+  const { queryByTestId } = render(<ToggleMessage />);
+
+  const toggle = queryByTestId("toggle");
+  expect(toggle).toBeInTheDocument();
+});
+
+test("Toggle message should be set data-testid='text'", () => {
+  const { queryByTestId } = render(<ToggleMessage />);
+
+  const toggle = queryByTestId("toggle");
+  fireEvent.click(toggle);
+
+  const text = queryByTestId("text");
+  expect(text).toBeInTheDocument();
+});
+
+test("Toggle message 'Want to buy a new car?' should be displayed on the page", () => {
+  const { getByText } = render(<ToggleMessage />);
+
+  const toggle = getByText("Want to buy a new car?");
+  expect(toggle).toBeInTheDocument();
+});
+
+test("At the start, 'Call +11 22 33 44 now!' should not be visible", () => {
+  const { queryByTestId } = render(<ToggleMessage />);
+  const text = queryByTestId("text");
+  expect(text).not.toBeInTheDocument();
+});
+
+test("After clicking toggle message, 'Call +11 22 33 44 now!' should be visible", () => {
+  const { queryByTestId } = render(<ToggleMessage />);
+
+  const toggle = queryByTestId("toggle");
+  fireEvent.click(toggle);
+
+  const text = queryByTestId("text");
+  expect(text).toBeInTheDocument();
+});
+
+test("After clicking toggle message twice, 'Call +11 22 33 44 now!' should not be visible", () => {
+  const { queryByTestId } = render(<ToggleMessage />);
+
+  const toggle = queryByTestId("toggle");
+  fireEvent.click(toggle);
+  fireEvent.click(toggle);
+
+  const text = queryByTestId("text");
+  expect(text).not.toBeInTheDocument();
 });
