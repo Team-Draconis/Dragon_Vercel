8e3a7f49dde59306b50cceadbd2bb2736675bb9e
Mako Kusuda <mako.kusuda@gmail.com>
2020-06-14 18:23:29 +0900

    Fix dockerfile and add error handling

--- codes_file.js
+++ codes_file.js
@@ -1,5 +1,5 @@
 import React from 'react'; function Codes() {
-  return (<div><p>Hellooo World!</p><button>Click</button></div>)
+  return (<div><p>Hello World!</p><button>Click</button></div>)
 }
 <Codes />
  export default Codes
