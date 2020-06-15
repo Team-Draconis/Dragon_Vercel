28aa3b48e82e5745f876ceeb87772e939a9356b7
cwcraw <cwcraw@gmail.com>
2020-06-15 19:21:28 +0900

    Finished Add City button on appl/dash

--- pages/api/addCity.js
+++ pages/api/addCity.js
@@ -12,7 +12,7 @@ export default async (req, res) => {
     case "POST":
       try {
         console.log(req.body)
-        Candidate.update({candidate_email:req.body.candidate_email},{candidate_city: req.body.candidate_city})
+        await Candidate.update({candidate_email:req.body.candidate_email},{$push: {candidate_city: req.body.candidate_city}})
         res.status(200)
       } catch (error) {
         res.status(400).json({ message: "Oops,Add City failed" });
