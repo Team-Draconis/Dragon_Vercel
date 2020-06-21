2a94f80b1777d563aa59d6a475b2df2050a181fc
maegatro <shuntaromaekawa@gmail.com>
2020-06-22 01:44:52 +0900

    style update

--- pages/AboutUs.js
+++ pages/AboutUs.js
@@ -33,122 +33,188 @@ export default function AboutUs() {
           animate={{ y: 0, opacity: 1 }}
           transition={{ ease: "easeOut", duration: 1.5, delay: 0.6 }}
         >
-          <Grid container spacing={1} align="center">
+          <Box mb={3}>
+            <Typography
+              variant="h4"
+              component="h1"
+              style={{ fontFamily: "Josefin Sans" }}
+            >
+              Our Team
+            </Typography>
+          </Box>
+          <Grid container spacing={6} align="center">
             <Grid item xs={12} sm={6} md={6}>
-              <img
-                src="/Zowie_thumb.jpeg"
-                alt="Zowie Min"
-                width="200"
-                height="230"
-                style={{ borderRadius: "50%" }}
-              />
-              <a href="https://www.linkedin.com/in/jiayi-min/">Zowie Min</a>
+              <Link href="https://www.linkedin.com/in/jiayi-min/">
+                <motion.img
+                  whileHover={{ scale: 1.1 }}
+                  src="/Zowie_thumb.jpeg"
+                  alt="Zowie Min"
+                  width="200"
+                  height="230"
+                  style={{ borderRadius: "50%" }}
+                />
+                <Grid item md={6}>
+                  <Typography
+                    variant="h5"
+                    component="h1"
+                    align="center"
+                    style={{ fontFamily: "Josefin Sans" }}
+                  >
+                    Zowie Min
+                  </Typography>
+                </Grid>
+              </Link>
             </Grid>
-            <Grid item xs={12} sm={6} md={6}>
-              <img
-                src="Tam_thumb.jpeg"
-                alt="Tam Nguyen"
-                width="200"
-                height="230"
-                style={{ borderRadius: "50%" }}
-              />
-              <a href="https://www.linkedin.com/in/tam-nguyen-70a7891a8/">
-                Tam Nugyen
-              </a>
+            <Grid item xs={12} sm={6} md={4}>
+              <Link href="https://www.linkedin.com/in/tam-nguyen-70a7891a8/">
+                <motion.img
+                  whileHover={{ scale: 1.1 }}
+                  src="Tam_thumb.jpeg"
+                  alt="Tam Nguyen"
+                  width="200"
+                  height="230"
+                  style={{ borderRadius: "50%", cursor: "pointer" }}
+                />
+                <Grid item md={6}>
+                  <Typography
+                    variant="h5"
+                    component="h1"
+                    align="center"
+                    style={{ fontFamily: "Josefin Sans" }}
+                  >
+                    Tam Nugyen
+                  </Typography>
+                </Grid>
+              </Link>
             </Grid>
 
             <Grid item xs={12} sm={6} md={4}>
-              <img
-                src="Shuntaro_thumb.jpg"
-                alt="Shuntaro Maekawa"
-                width="200"
-                height="230"
-                style={{ borderRadius: "50%" }}
-              />
-              <a href="https://www.linkedin.com/in/shuntaro-maekawa/">
-                Shuntaro Maekawa
-              </a>
+              <Link href="https://www.linkedin.com/in/shuntaro-maekawa/">
+                <motion.img
+                  whileHover={{ scale: 1.1 }}
+                  src="Shuntaro_thumb.jpg"
+                  alt="Shuntaro Maekawa"
+                  width="200"
+                  height="230"
+                  style={{ borderRadius: "50%", cursor: "pointer" }}
+                />
+                <Grid item md={8}>
+                  <Typography
+                    variant="h5"
+                    component="h1"
+                    align="center"
+                    style={{ fontFamily: "Josefin Sans" }}
+                  >
+                    Shuntaro Maekawa
+                  </Typography>
+                </Grid>
+              </Link>
             </Grid>
 
             <Grid item xs={12} sm={6} md={4}>
-              <img
-                src="Mako_thumb.jpg"
-                alt="Mako Kusuda"
-                width="200"
-                height="230"
-                style={{ borderRadius: "50%" }}
-              />
-              <a href="https://www.linkedin.com/in/makokusuda/">Mako Kusudo</a>
+              <Link href="https://www.linkedin.com/in/makokusuda/">
+                <motion.img
+                  whileHover={{ scale: 1.1 }}
+                  src="Mako_thumb.jpg"
+                  alt="Mako Kusuda"
+                  width="200"
+                  height="230"
+                  style={{ borderRadius: "50%", cursor: "pointer" }}
+                />
+                <Grid item md={6}>
+                  <Typography
+                    variant="h5"
+                    component="h1"
+                    align="center"
+                    style={{ fontFamily: "Josefin Sans" }}
+                  >
+                    Mako Kusudo
+                  </Typography>
+                </Grid>
+              </Link>
             </Grid>
             <Grid item xs={12} sm={12} md={4}>
-              <img
-                src="CC-photo_thumb.jpg"
-                alt="Chip Crawford"
-                width="200"
-                height="230"
-                style={{ borderRadius: "50%" }}
-              />
-              <a href="https://www.linkedin.com/in/chip-crawford-berkeley/">
-                Chip Crawford
-              </a>
+              <Link href="https://www.linkedin.com/in/chip-crawford-berkeley/">
+                <motion.img
+                  whileHover={{ scale: 1.1 }}
+                  src="CC-photo_thumb.jpg"
+                  alt="Chip Crawford"
+                  width="200"
+                  height="230"
+                  style={{ borderRadius: "50%", cursor: "pointer" }}
+                />
+                <Grid item md={6}>
+                  <Typography
+                    variant="h5"
+                    component="h1"
+                    align="center"
+                    style={{ fontFamily: "Josefin Sans" }}
+                  >
+                    Chip Crawford
+                  </Typography>
+                </Grid>
+              </Link>
             </Grid>
           </Grid>
         </motion.div>
         {/* story */}
-        <Typography
-          variant="body1"
-          component="h1"
-          align="center"
-          style={{ fontFamily: "Josefin Sans" }}
-        >
-          <motion.div
-            initial={{ y: 26 * 1.2, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1.5, delay: 0.6 }}
+        <Box mt={5}>
+          <Typography
+            variant="body1"
+            component="h1"
+            align="center"
+            style={{ fontFamily: "Josefin Sans" }}
           >
-            Coding Quiz
-            <a href="https://github.com/Team-Draconis/Dragon_Vercel">
-              {"Dragon Tester"}
-            </a>{" "}
-            was developed by{" "}
-            <a href="https://www.linkedin.com/in/makokusuda/">Mako Kusudo</a>,{" "}
-            <a href="https://www.linkedin.com/in/shuntaro-maekawa/">
-              Shuntaro Maekawa
-            </a>
-            , <a href="https://www.linkedin.com/in/jiayi-min/">Zowie Min</a>,{" "}
-            <a href="https://www.linkedin.com/in/tam-nguyen-70a7891a8/">
-              {" "}
-              Tam Nugyen
-            </a>{" "}
-            and{" "}
-            <a href="https://www.linkedin.com/in/chip-crawford-berkeley/">
-              {" "}
-              Chip Crawford
-            </a>{" "}
-            during their time at{" "}
-            <a href="https://www.codechrysalis.io/">
-              {" "}
-              Code Chrysalis Coding Boot Camp
-            </a>{" "}
-            in Tokyo, Japan. We were inspired by Zowie's experience as a
-            recruiter to try to improve the experience for both the companies
-            looking for talent and the talent themselves. Our goal was to allow
-            jobseekers to lead with their skills and connect them with small to
-            medium sized companies that may have been overlooked.
-            <br />
-            Our focus was on creating a stable platform and refining the skills
-            we learned at Code Chrysalis. Mako was in charge of using Docker and
-            deploying the app to AWS, she also supported test development. Zowie
-            was in charge of Database management and worked on user
-            authentication with Tam. Tam helped on the backend, login and test
-            development. Shuntaro created the test editor and was our frontend
-            guru. Finally, Chip was the tech lead. He helped the team meet
-            deadlines, performed QA tasks and wrote small features as needed.
-            <br />
-            We hope our app can inspire your future projects and lead to a
-            better way for people to showcase their talents!
-          </motion.div>
-        </Typography>
+            <motion.div
+              initial={{ y: 26 * 1.2, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 1.5, delay: 0.6 }}
+            >
+              Coding Quiz
+              <a href="https://github.com/Team-Draconis/Dragon_Vercel">
+                {"Dragon Tester"}
+              </a>{" "}
+              was developed by{" "}
+              <a href="https://www.linkedin.com/in/makokusuda/">Mako Kusudo</a>,{" "}
+              <a href="https://www.linkedin.com/in/shuntaro-maekawa/">
+                Shuntaro Maekawa
+              </a>
+              , <a href="https://www.linkedin.com/in/jiayi-min/">Zowie Min</a>,{" "}
+              <a href="https://www.linkedin.com/in/tam-nguyen-70a7891a8/">
+                {" "}
+                Tam Nugyen
+              </a>{" "}
+              and{" "}
+              <a href="https://www.linkedin.com/in/chip-crawford-berkeley/">
+                {" "}
+                Chip Crawford
+              </a>{" "}
+              during their time at{" "}
+              <a href="https://www.codechrysalis.io/">
+                {" "}
+                Code Chrysalis Coding Boot Camp
+              </a>{" "}
+              in Tokyo, Japan. We were inspired by Zowie's experience as a
+              recruiter to try to improve the experience for both the companies
+              looking for talent and the talent themselves. Our goal was to
+              allow jobseekers to lead with their skills and connect them with
+              small to medium sized companies that may have been overlooked.
+              <br />
+              Our focus was on creating a stable platform and refining the
+              skills we learned at Code Chrysalis. Mako was in charge of using
+              Docker and deploying the app to AWS, she also supported test
+              development. Zowie was in charge of Database management and worked
+              on user authentication with Tam. Tam helped on the backend, login
+              and test development. Shuntaro created the test editor and was our
+              frontend guru. Finally, Chip was the tech lead. He helped the team
+              meet deadlines, performed QA tasks and wrote small features as
+              needed.
+              <br />
+              We hope our app can inspire your future projects and lead to a
+              better way for people to showcase their talents!
+            </motion.div>
+          </Typography>
+        </Box>
       </Container>
     </>
   );
